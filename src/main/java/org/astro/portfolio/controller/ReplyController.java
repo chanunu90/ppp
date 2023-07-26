f63732659439c5f7e903582fc3b550c3d8a12e17
himediaEUNWOOCHA <vltfhem1206@gmail.com>
2023-07-26 19:30:49 +0900

    0726

---
+++ src/main/java/org/astro/portfolio/controller/ReplyController.java
@@ -0,0 +1,13 @@
+package org.astro.portfolio.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@RestController
+@Log4j2
+@RequiredArgsConstructor
+public class ReplyController {
+    
+}
